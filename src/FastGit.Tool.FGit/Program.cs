d27fbaff077fa5944829108658e5e7cc3bf13d3e
Rwing <rwing@rwing.cn>
2020-09-25 17:23:43 +0800

    Add project files.

---
+++ src/FastGit.Tool.FGit/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using CliWrap;
+using CliWrap.Buffered;
+using CliWrap.EventStream;
+
+namespace FastGit.Tool.FGit
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length > 1 && args[0] == "clone")
+            {
+                var repoUriIndex = FindRepoUriIndex(args);
+                if (repoUriIndex > 0)
+                {
+                    args[repoUriIndex] = args[repoUriIndex].Replace("://github.com/", "://hub.fastgit.org/", StringComparison.OrdinalIgnoreCase);
+                }
+
+                if (NoProgressOption(args))
+                {
+                    Array.Resize(ref args, args.Length + 1);
+                    Array.Copy(args, 1, args, 2, args.Length - 2);
+                    args[1] = "--progress";
+                }
+            }
+            using var stdOut = Console.OpenStandardOutput();
+            using var stdErr = Console.OpenStandardError();
+            var cmd = Cli.Wrap("git").WithArguments(string.Join(' ', args)).WithValidation(CommandResultValidation.None) | (stdOut, stdErr);
+            await cmd.ExecuteAsync();
+        }
+
+        private static bool NoProgressOption(string[] args)
+        {
+            return Array.FindIndex(args, i => i == "--progress") < 0 && Array.FindIndex(args, i => i == "-q") < 0;
+        }
+
+        private static int FindRepoUriIndex(string[] args)
+        {
+            return Array.FindIndex(args, i => i.StartsWith("http"));
+        }
+    }
+}
---
+++ test/FastGit.Tool.FGit.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FastGit.Tool.FGit.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
